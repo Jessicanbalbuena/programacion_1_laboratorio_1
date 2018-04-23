c465a8f44c22ae46b5be0ddf846868fd721a77f5
Jessica <36174668+Jessicanbalbuena@users.noreply.github.com>
2018-04-23 20:34:00 -0300

    Add files via upload

---
+++ matrices/main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    float matriz[2][3];
+int i,j;
+for(i=0;i<2;i++)
+{
+for(j=0;j<3;j++)
+{
+printf("M[%d][%d]: ",i,j);
+scanf("%f",&matriz[i][j]);
+}
+}
+for (i=0; i <2; i++){
+    for (j=0; j <3; i++){
+        printf("%.2f\t", matriz[i][j]);
+    }
+    printf("\n");
+}
+return 0;
+}
+
+
+
+
